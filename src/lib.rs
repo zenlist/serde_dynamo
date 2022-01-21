0d327d98507b0f9a56aa39070a7089bc471a8cd2
David Young <david@thedavidyoung.co.uk>
2022-01-21 17:33:29 +0000

    Update aws_sdk to 0.5

--- src/lib.rs
+++ src/lib.rs
@@ -102,10 +102,10 @@
 //!
 //! ```toml
 //! [dependencies]
-//! serde_dynamo = { version = "3", features = ["aws-sdk-dynamodb+0_4"] }
+//! serde_dynamo = { version = "3", features = ["aws-sdk-dynamodb+0_5"] }
 //! ```
 //!
-//! See [`aws_sdk_dynamodb_0_4`] for examples and more information.
+//! See [`aws_sdk_dynamodb_0_5`] for examples and more information.
 //!
 //!
 //! ## rusoto support
@@ -241,6 +241,12 @@ aws_sdk_macro!(
     feature = "aws-sdk-dynamodb+0_4",
     crate_name = aws_sdk_dynamodb_0_4,
     aws_version = "0.4",
+);
+
+aws_sdk_macro!(
+    feature = "aws-sdk-dynamodb+0_5",
+    crate_name = aws_sdk_dynamodb_0_5,
+    aws_version = "0.5",
 );
 
 rusoto_macro!(
